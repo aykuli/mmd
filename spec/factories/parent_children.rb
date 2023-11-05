78cd84a7d47748265930c893c02e437ab2a06ba8
aynur <aykuli@ya.ru>
2023-11-05 23:04:52 +0700

    rubocop

--- spec/factories/parent_children.rb
+++ spec/factories/parent_children.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory(:parent_child_relation, class: "ParentsChildren") do
+  factory(:parent_child_relation, class: 'ParentsChildren') do
     association :parent, factory: :user
     association :child, factory: :user
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User do
   subject(:user) { create(:user) }
 
-  describe '.new' do |example|
+  describe '.new' do |_example|
     byebug
     it 'returns parent like user' do
       expect(user).to respond_to(:sessions, :children)
